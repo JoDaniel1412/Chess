904d408bb17f74076f7cc3b57dcb4a3ffac945bc
Jose Acuña <jodaniel1409@gmail.com>
2019-11-22 11:47:34 -0600

    [INIT] Board tiles generation

---
+++ Assets/Scripts/Board/Tile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Board
+{
+    public class Tile : MonoBehaviour
+    {
+        private int _i;
+        private int _j;
+        
+        public void Index(int i, int j)
+        {
+            _i = i;
+            _j = j;
+        }
+
+        public int I => _i;
+
+        public int J => _j;
+    }
+}
---
+++ Assets/Scripts/Board/Tilemap.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Board
+{
+    public class Tilemap : MonoBehaviour
+    {
+        public int rows;
+        public int columns;
+        public float offset;
+        public Transform tilePref;
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+            LoadGrid();
+        }
+
+        // Instantiates each tile of the grid
+        private void LoadGrid()
+        {
+            var tileSize = tilePref.GetComponent<MeshRenderer>().bounds.size;
+            var xOffset = tileSize.x + offset;
+            var zOffset = tileSize.z + offset;
+            
+            for (var i = 0; i < rows; i++)
+            {
+                var z = zOffset * i;
+                for (var j = 0; j < columns; j++)
+                {
+                    var x = xOffset * j;
+                    var poss = new Vector3(x, 0, z);
+                    var tile = Instantiate(tilePref, poss, Quaternion.identity);
+                    tile.SetParent(transform);
+                    tile.GetComponent<Tile>().Index(i, j);
+                }
+            }
+        }
+    }
+}
