a0312b39d938ee6cba1b7318aba32ecb8411eacc
Jose Acuña <jodaniel1409@gmail.com>
2019-11-22 17:22:54 -0600

    [UPDATE] Tiles colors and highlight

--- Assets/Scripts/Board/Tile.cs
+++ Assets/Scripts/Board/Tile.cs
@@ -1,16 +1,37 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 namespace Board
 {
     public class Tile : MonoBehaviour
     {
+        public Material highlight; 
+            
         private int _i;
         private int _j;
+        private Material _defaultMaterial;
+        
         
         public void Index(int i, int j)
         {
             _i = i;
             _j = j;
+        }
+        
+        // Highlights the Tile when the mouse enters
+        private void OnMouseEnter()
+        {
+            var mesh = GetComponent<MeshRenderer>();
+            _defaultMaterial = mesh.material;
+            mesh.material = highlight;
+        }
+
+        // Removes the highlight of the Tile when mouse exit
+        private void OnMouseExit()
+        {
+            var mesh = GetComponent<MeshRenderer>();
+            mesh.material = _defaultMaterial;
         }
 
         public int I => _i;
--- Assets/Scripts/Board/Tilemap.cs
+++ Assets/Scripts/Board/Tilemap.cs
@@ -1,7 +1,10 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEditor;
+using UnityEngine;
 
 namespace Board
 {
+    [ExecuteInEditMode]
     public class Tilemap : MonoBehaviour
     {
         public int rows;
@@ -9,10 +12,21 @@ namespace Board
         public float offset;
         public Transform tilePref;
         
-        // Start is called before the first frame update
+        
         private void Start()
         {
             LoadGrid();
+        }
+
+        private void OnEnable()
+        {
+            DestroyGrid();
+            LoadGrid();
+        }
+
+        private void OnDisable()
+        {
+            DestroyGrid();
         }
 
         // Instantiates each tile of the grid
@@ -21,6 +35,7 @@ namespace Board
             var tileSize = tilePref.GetComponent<MeshRenderer>().bounds.size;
             var xOffset = tileSize.x + offset;
             var zOffset = tileSize.z + offset;
+            var count = 0;
             
             for (var i = 0; i < rows; i++)
             {
@@ -30,10 +45,34 @@ namespace Board
                     var x = xOffset * j;
                     var poss = new Vector3(x, 0, z);
                     var tile = Instantiate(tilePref, poss, Quaternion.identity);
-                    tile.SetParent(transform);
-                    tile.GetComponent<Tile>().Index(i, j);
+                    count++;
+                    SetupTile(tile, i, j, count);
                 }
+
+                count--;
             }
+        }
+        
+        // Deletes the Grid
+        private static void DestroyGrid()
+        {
+            var tiles = GameObject.FindGameObjectsWithTag("Tile");
+            foreach (var children in tiles)
+                DestroyImmediate(children);
+        }
+
+        // Selects the color for the Tile and other properties
+        private void SetupTile(Transform tile, int i, int j, int count)
+        {
+            tile.SetParent(transform);
+            tile.GetComponent<Tile>().Index(i, j);
+            
+            // Material
+            const string materialsPath = "Assets/Materials/Board/";
+            var path = materialsPath + "BlackTile.mat";
+            if (count % 2 == 0) path = materialsPath + "WhiteTile.mat";
+            var material = AssetDatabase.LoadAssetAtPath<Material>(path);
+            tile.GetComponent<MeshRenderer>().sharedMaterial = material;
         }
     }
 }
