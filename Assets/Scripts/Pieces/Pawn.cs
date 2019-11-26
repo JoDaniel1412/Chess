91bed8affb197014a320c6debcca3c15dc7a6911
Jose Acuña <jodaniel1409@gmail.com>
2019-11-26 10:01:35 -0600

    [FIXED] Pawn movement and occupied Tiles

--- Assets/Scripts/Pieces/Pawn.cs
+++ Assets/Scripts/Pieces/Pawn.cs
@@ -9,23 +9,25 @@ namespace Pieces
         
         public override List<Vector2Int> Movements()
         {
+            var result = new List<Vector2Int>();
+            var occupied = PiecesManager.GetOccupied();
             
             // Direction of Movement
             var dir = 1;
             if (team.Equals(Team.Black)) dir = -1;
-            
-            
-            // Checks collisions
-            var occupied = PiecesManager.GetOccupied();
             var i = poss.x + dir;
-            var vect2 = new Vector2Int(i, poss.y);
 
-            if (occupied.Contains(vect2)) return new List<Vector2Int>();
+            // Checks collisions
+            var vect = new Vector2Int(i, poss.y);
+            if (occupied.Contains(vect)) return result;
+            result.Add(vect);
             
             // First extra step
-            if (_firstMove && !occupied.Contains(new Vector2Int(i + dir, poss.y))) i += dir;
+            var vect2 = new Vector2Int(i + dir, poss.y);
+            if (_firstMove && !occupied.Contains(vect2)) 
+                result.Add(vect2);
             
-            return new List<Vector2Int> {vect2};
+            return result;
         }
 
         protected override void Move(Vector2Int newPoss)
